8986457680b5026b7480c43d2de6a58933cebad9
mnegi05 <manishnegi572@gmail.com>
2026-04-23 23:16:43 +0530

    update app main file

--- src/app.js
+++ src/app.js
@@ -1,38 +1,27 @@
-// app.js
-import express, { json } from "express";
+import express from "express";
 const app = express();
 
-app.use(json());
+const connectDB = require("./config/db");
+const limiter = require("./middlewares/rateLimiter");
+const errorHandler = require("./middlewares/errorHandler");
 
-// Imports
-import Order from "./models/Order";
-import OrderRepository from "./repositories/OrderRepository";
+const container = require("./container/container");
+const TYPES = require("./container/types");
 
-import OrderService from "./services/OrderService";
-import PaymentService from "./services/PaymentService";
-import NotificationService from "./services/NotificationService";
+app.use(express.json());
+app.use(limiter);
 
-import UpiPayment from "./implementations/UpiPayment";
-import EmailNotification from "./implementations/EmailNotification";
+// DB
+connectDB();
 
-// Dependency Injection
-const orderRepo = new OrderRepository();
-
-const paymentMethod = new UpiPayment();
-const paymentService = new PaymentService(paymentMethod);
-
-const notificationMethod = new EmailNotification();
-const notificationService = new NotificationService(notificationMethod);
-
-const orderService = new OrderService(
-    orderRepo,
-    paymentService,
-    notificationService
-);
+// DI
+const orderService = container.get(TYPES.OrderService);
 
 // Controller
 const orderController = require("./controllers/OrderController")(orderService);
 app.use("/", orderController);
 
-// Start server
-app.listen(3000, () => console.log("Server running on port 3000"));
+// Error Handler (last)
+app.use(errorHandler);
+
+app.listen(3000, () => console.log("Server running"));
