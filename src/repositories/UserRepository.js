a6e003b4500f32f9506d91adbe6fc7ad93431a53
mnegi05 <manishnegi572@gmail.com>
2026-04-24 10:01:08 +0530

    user based repository

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,13 @@
+import prisma from "../config/prisma";
+
+class UserRepository {
+    async create(user) {
+        return prisma.user.create({ data: user });
+    }
+
+    async findByEmail(email) {
+        return prisma.user.findUnique({ where: { email } });
+    }
+}
+
+export default UserRepository;
